b8ae737135c10ef49918fc428702b627367a9951
Maja Thunberg <majaneko@gmail.com>
2022-04-30 17:11:11 +0200

    Validate input and form

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,19 +1,48 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
+import { validateLon, validateLat } from '../../utils/validate'
 
 export const Form = ({ onSubmit }) => {
   const [userInput, setUserInput] = useState({
     longitude: '',
     latitude: ''
   })
+  const [isValidInput, setIsValidInput] = useState({
+    longitude: false,
+    latitude: false
+  })
+  const [isValidForm, setIsValidForm] = useState(false)
+
+  useEffect(() => {
+    // validate input on change
+    const isValid = validateLon(userInput.longitude)
+    setIsValidInput((prevState) => {
+      return {...prevState,
+        longitude: isValid
+      }})
+    }, [userInput.longitude])
+    
+  useEffect(() => {
+    const isValid = validateLat(userInput.latitude)
+    setIsValidInput((prevState) => {
+      return {...prevState,
+        latitude: isValid
+      }})
+  }, [userInput.latitude])
+    
+  useEffect(() => {
+    // validate form on input validation change
+    setIsValidForm(isValidInput.longitude && isValidInput.latitude)
+  }, [isValidInput])
 
   function handleInputChange(e) {
+    // update state with input name: value
     setUserInput((prevInput) => { 
       return {...prevInput, 
         [e.target.name]: e.target.value
       }})
   }
 
-  async function handleSubmit(e) {
+  function handleSubmit(e) {
     e.preventDefault()
     onSubmit(userInput)
   }
@@ -37,6 +66,7 @@ export const Form = ({ onSubmit }) => {
         </label>
         <button
           onClick={handleSubmit}
+          disabled={!isValidForm}
         >
           Get weather
         </button>
---
+++ src/utils/validate.js
@@ -0,0 +1,9 @@
+export const validateLon = (value) => {
+ const regex = new RegExp('^(\\+|-)?(?:180(?:(?:\\.0{1,6})?)|(?:[0-9]|[1-9][0-9]|1[0-7][0-9])(?:(?:\\.[0-9]{1,6})?))$')
+ return regex.test(value)
+}
+
+export const validateLat = (value) => {
+  const regex = new RegExp('^(\\+|-)?(?:90(?:(?:\\.0{1,6})?)|(?:[0-9]|[1-8][0-9])(?:(?:\\.[0-9]{1,6})?))$')
+  return regex.test(value)
+}
